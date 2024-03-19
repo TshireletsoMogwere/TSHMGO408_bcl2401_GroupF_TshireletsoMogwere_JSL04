2f8254f74c2b3d727562d19f01f28e8038cd8f90
TshireletsoMogwere <tmogwere09@gmail.com>
2024-03-19 10:34:40 +0200

    Did a flabour selection, stock check and workout intensity by consoling the needed outputs

--- index.js
+++ index.js
@@ -5,22 +5,31 @@
 // print whether the user prefers a "Regular" or "Sugar-free" variety. If the preference is not set, default to "Regular".
 
 let userPreference = 'Sugar-free'; // Possible values: 'Regular', 'Sugar-free', or undefined
-console.log(``);
+console.log(`Sugar-free`);
 
 // #### Challenge 2: Stock Check
 // You have a variable that tracks the number of Monster Energy cans left in the fridge. 
 // Use a ternary operator to print "Time to restock!" if the number is less than 5, or "We're stocked!" if the number is 5 or more.
 
 let cansLeft = 3; // Any number of cans
-console.log(``);
+if (cansLeft < 5) {
+console.log("Time to restock!");
+} 
 
+if (cansLeft <= 5) {
+    console.log("We're stocked!")
+}
 
 // #### Challenge 3: Workout Intensity
 // Based on the current heart rate, determine if the user should drink a Monster Energy drink to boost their workout. 
 // If the heart rate is below 100 bpm, print "Boost needed!", otherwise, print "Energy levels are high!".
 
 let heartRate = 95; // Current heart rate in bpm
-console.log(`'}`);
+if (heartRate < 100) {
+console.log("Boost needed!");
+} else {
+    console.log("Energy levels are high!");
+}
 
 
 // #### Challenge 4: Temperature Suitability
