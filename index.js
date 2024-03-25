ab7d84d68f2ea3613f951f9e36a8f0b01baab6a6
TshireletsoMogwere <tmogwere09@gmail.com>
2024-03-25 12:52:49 +0200

    modified code

--- index.js
+++ index.js
@@ -4,15 +4,16 @@
 // Given a variable that holds a user's preferred type of Monster Energy drink, 
 // print whether the user prefers a "Regular" or "Sugar-free" variety. If the preference is not set, default to "Regular".
 
-   let userPreference = "Sugar-free"; // Possible values: 'Regular', 'Sugar-free', or undefined
-      console.log(userPreference === "Sugar-free" ? "Sugar-free" : userPreference === "Regular" ? "Regular": "Regular");
+    let userPreference = "Sugar-free" // Possible values: 'Regular', 'Sugar-free', or undefined
+      console.log(userPreference === "Sugar-free" ? "Sugar-free" : "Regular"); 
+     
 
 // #### Challenge 2: Stock Check
 // You have a variable that tracks the number of Monster Energy cans left in the fridge. 
 // Use a ternary operator to print "Time to restock!" if the number is less than 5, or "We're stocked!" if the number is 5 or more.
 
    let cansLeft = 7; // Any number of cans
-      console.log (cansLeft <= 5 ? "Time to restock!" : "We're stocked!");
+      console.log (cansLeft <= 5 ? "Time to restock!" :  "We're stocked!");
 // #### Challenge 3: Workout Intensity
 // Based on the current heart rate, determine if the user should drink a Monster Energy drink to boost their workout. 
 // If the heart rate is below 100 bpm, print "Boost needed!", otherwise, print "Energy levels are high!".
